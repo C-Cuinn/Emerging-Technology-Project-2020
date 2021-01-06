6a75a030927cc1215336820c5cb8c498343baea5
CIARAN QUINN <G00361840@gmit.ie>
2021-01-06 16:54:00 +0000

    Comments , began integrating docker and flask, added to the README

---
+++ flask.py
@@ -0,0 +1,19 @@
+import flask as fl
+
+import numpy as np
+
+app = fl.Flask(__name__)
+
+@app.route("/")
+def home(): # 
+  return app.send_static_file('index.html')
+
+@app.route('/power_gen_lin')
+def power_gen_lin():
+    return {}
+
+@app.route('/power_gen_pol')
+def power_gen_pol():
+    return {}
+    
+
